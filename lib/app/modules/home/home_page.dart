5c5f1e51f81209f7c342850a6342fc1d08a4b5a3
SamuelXimenes <samuel.ximenes.oliveira60@aluno.ifce.edu.br>
2023-07-22 19:40:24 +0100

    Refactored home page for getting user data with blocs

--- lib/app/modules/home/home_page.dart
+++ lib/app/modules/home/home_page.dart
@@ -1,11 +1,13 @@
 import 'dart:developer';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:expenses/app/bloc/transaction_bloc.dart';
 import 'package:expenses/app/modules/home/components/filter_cards.dart';
 import 'package:expenses/app/shared/widgets/appbar/appbar_title_widget.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
+import '../../bloc/user_authentication_bloc.dart';
+import '../../bloc/utils_bloc.dart';
 import 'components/card_expense_money.dart';
 import 'components/card_expense_list.dart';
 
@@ -17,36 +19,19 @@ class ExpenseHomePage extends StatefulWidget {
 }
 
 class _ExpenseHomePageState extends State<ExpenseHomePage> {
+  final transactionBloc = TransactionBloC();
+  final authBloc = AuthenticationBloC();
+  final utils = Utils();
+
   Query? collectionsInstance;
-  String? documentId;
-  String? transactionsCollectionReference;
+  String? documentId = '';
+  String? userNameCollection = '';
+  Future<UserData>? userData;
 
   @override
   void initState() {
     super.initState();
-    _fetchTransactions();
-  }
-
-  Future<void> _fetchTransactions() async {
-    User? user = FirebaseAuth.instance.currentUser;
-    String? userUID = user?.uid;
-
-    final querySnapshot = await FirebaseFirestore.instance
-        .collection('transactionsProduction')
-        .where('allowedUsers', arrayContainsAny: [userUID]).get();
-
-    if (querySnapshot.docs.isNotEmpty) {
-      documentId = querySnapshot.docs.first.id;
-      log(documentId!);
-
-      collectionsInstance = FirebaseFirestore.instance
-          .collection('transactionsProduction')
-          .doc(documentId)
-          .collection('samuel_sabrina_transactionsCollection')
-          .orderBy('date', descending: true);
-    } else {
-      log('Usuário não tem permissão para acessar nenhum item');
-    }
+    userData = transactionBloc.fetchTransactions();
   }
 
   @override
@@ -58,64 +43,96 @@ class _ExpenseHomePageState extends State<ExpenseHomePage> {
           preferredSize: size,
           child: const CustomAppBarWidgetTitle(title: 'Despesas Pessoais')),
       body: SingleChildScrollView(
-        child: Column(
-          children: [
-            const ExpenseCardMoney(),
-            FilterCarousel(
-              onFilterSelected: (value) {
-                if (value == 'Geral') {
-                  setState(() {
-                    collectionsInstance = FirebaseFirestore.instance
-                        .collection('transactionsProduction')
-                        .doc(documentId)
-                        .collection('samuel_sabrina_transactionsCollection')
-                        .orderBy('date', descending: true);
-                  });
-                } else if (value == 'Comida') {
-                  setState(() {
-                    collectionsInstance = FirebaseFirestore.instance
-                        .collection('transactionsProduction')
-                        .doc(documentId)
-                        .collection('samuel_sabrina_transactionsCollection')
-                        .where('type', isEqualTo: 'Comida')
-                        .orderBy('date', descending: true);
-                  });
-                } else if (value == 'Roupa') {
-                  setState(() {
-                    collectionsInstance = FirebaseFirestore.instance
-                        .collection('transactionsProduction')
-                        .doc(documentId)
-                        .collection('samuel_sabrina_transactionsCollection')
-                        .where('type', isEqualTo: 'Roupa')
-                        .orderBy('date', descending: true);
-                  });
-                } else if (value == 'Ganhos') {
-                  setState(() {
-                    collectionsInstance = FirebaseFirestore.instance
-                        .collection('transactionsProduction')
-                        .doc(documentId)
-                        .collection('samuel_sabrina_transactionsCollection')
-                        .where('type', isEqualTo: 'Ganhos')
-                        .orderBy('date', descending: true);
-                  });
-                } else if (value == 'Outros') {
-                  setState(() {
-                    collectionsInstance = FirebaseFirestore.instance
-                        .collection('transactionsProduction')
-                        .doc(documentId)
-                        .collection('samuel_sabrina_transactionsCollection')
-                        .where('type', isEqualTo: 'Outros')
-                        .orderBy('date', descending: true);
-                  });
-                }
-              },
-            ),
-            if (collectionsInstance != null) ...[
-              ExpenseCardList(
-                expenseCollection: collectionsInstance!,
-              ),
-            ],
-          ],
+        child: FutureBuilder<UserData>(
+          future: userData,
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Padding(
+                padding: EdgeInsets.only(top: size.height * 0.4),
+                child: const Column(
+                  children: [
+                    Center(
+                      child: CircularProgressIndicator(),
+                    ),
+                  ],
+                ),
+              );
+            } else if (snapshot.hasError) {
+              return Center(
+                child: Padding(
+                  padding: EdgeInsets.only(top: size.height * 0.25),
+                  child: const Column(
+                    children: [
+                      Icon(
+                        Icons.error,
+                        color: Colors.red,
+                      ),
+                      Text('Erro ao carregar os dados'),
+                    ],
+                  ),
+                ),
+              );
+            } else if (snapshot.hasData) {
+              UserData data = snapshot.data!;
+
+              return Column(
+                children: [
+                  const ExpenseCardMoney(),
+                  FilterCarousel(
+                    onFilterSelected: (value) {
+                      if (value == 'Geral') {
+                        log(data.collectionName!);
+                        log(data.transactionsCollectionName!);
+                        data.collectionsInstance;
+                      } else if (value == 'Comida') {
+                        setState(() {
+                          collectionsInstance = FirebaseFirestore.instance
+                              .collection('transactionsProduction')
+                              .doc(data.collectionName)
+                              .collection(data.transactionsCollectionName!)
+                              .where('type', isEqualTo: 'Comida')
+                              .orderBy('date', descending: true);
+                        });
+                      } else if (value == 'Roupa') {
+                        setState(() {
+                          collectionsInstance = FirebaseFirestore.instance
+                              .collection('transactionsProduction')
+                              .doc(data.collectionName)
+                              .collection(data.transactionsCollectionName!)
+                              .where('type', isEqualTo: 'Roupa')
+                              .orderBy('date', descending: true);
+                        });
+                      } else if (value == 'Ganhos') {
+                        setState(() {
+                          collectionsInstance = FirebaseFirestore.instance
+                              .collection('transactionsProduction')
+                              .doc(data.collectionName)
+                              .collection(data.transactionsCollectionName!)
+                              .where('type', isEqualTo: 'Ganhos')
+                              .orderBy('date', descending: true);
+                        });
+                      } else if (value == 'Outros') {
+                        setState(() {
+                          collectionsInstance = FirebaseFirestore.instance
+                              .collection('transactionsProduction')
+                              .doc(data.collectionName)
+                              .collection(data.transactionsCollectionName!)
+                              .where('type', isEqualTo: 'Outros')
+                              .orderBy('date', descending: true);
+                        });
+                      }
+                    },
+                  ),
+                  if (collectionsInstance != null) ...[
+                    ExpenseCardList(
+                      expenseCollection: collectionsInstance!,
+                    ),
+                  ],
+                ],
+              );
+            }
+            return Container();
+          },
         ),
       ),
     );
